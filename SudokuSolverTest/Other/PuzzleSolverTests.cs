561e99116756479dc9b5a3bb5934dee098dc21d3
davemlacroix <davemlacroix@gmail.com>
2018-07-06 10:25:54 -0400

    added second sudoku solver test

--- SudokuSolverTest/Other/PuzzleSolverTests.cs
+++ SudokuSolverTest/Other/PuzzleSolverTests.cs
@@ -32,7 +32,36 @@ namespace SudokuSolverTest.Other
             solver.Solve();
             printPuzzle.Print();
 
-            Assert.IsTrue(acts.Execute(new ValidateSection()));           
+            Assert.IsTrue(acts.Execute(new ValidateSection()));
+        }
+
+        [Test]
+        [Ignore("skip")]
+        public void Solve_Puzzle2_SolvesPuzzle()
+        {
+
+            var puzzle = new Puzzle(new int[9, 9]
+                {
+                    { 0, 7, 1, 0, 9, 0, 8, 0, 0},
+                    { 0, 0, 0, 3, 0, 6, 0, 0, 0},
+                    { 4, 9, 0, 0, 0, 0, 7, 0, 5},
+                    { 0, 1, 0, 9, 0, 0, 0, 0, 0},
+                    { 9, 0, 2, 0, 0, 0, 6, 0, 3},
+                    { 0, 0, 0, 0, 0, 8, 0, 2, 0},
+                    { 8, 0, 5, 0, 0, 0, 0, 7, 6},
+                    { 0, 0, 0, 6, 0, 7, 0, 0, 0},
+                    { 0, 0, 7, 0, 4, 0, 3, 5, 0},
+                });
+
+            var solver = new PuzzleSolver(puzzle);
+            var acts = new ActOnAllSegments(puzzle);
+            var printPuzzle = new PrintPuzzle(puzzle);
+
+            solver.Solve();
+            printPuzzle.Print();
+
+            Assert.IsTrue(acts.Execute(new ValidateSection()));
         }
     }
 }
+
