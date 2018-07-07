98c43aa77577125c48fa73bc2b3525707046d1ec
David L <davemlacroix@gmail.com>
2018-07-07 13:21:12 -0400

    added additional PuzzleSolver tests

--- SudokuSolverTest/Other/PuzzleSolverTests.cs
+++ SudokuSolverTest/Other/PuzzleSolverTests.cs
@@ -11,7 +11,6 @@ namespace SudokuSolverTest.Other
         [Test]
         public void Solve_Puzzle1_SolvesPuzzle()
         {
-
             var puzzle = new Puzzle(new int[9, 9]
                 {
                     { 0, 0, 3, 0, 2, 0, 6, 0, 0},
@@ -25,21 +24,12 @@ namespace SudokuSolverTest.Other
                     { 0, 0, 5, 0, 1, 0, 3, 0, 0}
                 });
 
-            var solver = new PuzzleSolver(puzzle);
-            var acts = new ActOnAllSegments(puzzle);
-            var printPuzzle = new PrintPuzzle(puzzle);
-
-            solver.Solve();
-            printPuzzle.Print();
-
-            Assert.IsTrue(acts.Execute(new ValidateSection()));
+            Solve_WithGivenPuzzle_SolvesPuzzle(puzzle);
         }
 
         [Test]
         public void Solve_Puzzle2_SolvesPuzzle()
         {
-
-            
             var puzzle = new Puzzle(new int[9, 9]
                 {
                     { 0, 7, 1, 0, 9, 0, 8, 0, 0},
@@ -53,6 +43,49 @@ namespace SudokuSolverTest.Other
                     { 0, 0, 7, 0, 4, 0, 3, 5, 0},
                 });
 
+            Solve_WithGivenPuzzle_SolvesPuzzle(puzzle);
+        }
+
+        [Test]
+        public void Solve_Puzzle3_SolvesPuzzle()
+        {
+            var puzzle = new Puzzle(new int[9, 9]
+                {
+                    { 7, 1, 0, 0, 0, 0, 0, 9, 0},
+                    { 0, 4, 0, 0, 1, 0, 0, 3, 0},
+                    { 0, 0, 0, 0, 7, 2, 0, 6, 0},
+                    { 9, 0, 0, 0, 0, 4, 0, 0, 5},
+                    { 0, 3, 5, 1, 0, 0, 9, 0, 0},
+                    { 0, 0, 0, 5, 0, 0, 6, 0, 8},
+                    { 1, 0, 0, 2, 0, 9, 7, 0, 0},
+                    { 6, 0, 9, 0, 0, 0, 0, 8, 0},
+                    { 0, 0, 7, 6, 4, 0, 0, 0, 9},
+                });
+
+            Solve_WithGivenPuzzle_SolvesPuzzle(puzzle);
+        }
+
+        [Test]
+        public void Solve_Puzzle4_SolvesPuzzle()
+        {
+            var puzzle = new Puzzle(new int[9, 9]
+                {
+                    { 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                    { 4, 0, 0, 6, 0, 9, 0, 0, 1},
+                    { 3, 1, 0, 0, 0, 0, 0, 4, 8},
+                    { 6, 0, 0, 4, 0, 7, 0, 0, 3},
+                    { 0, 0, 7, 0, 1, 0, 5, 0, 0},
+                    { 9, 0, 0, 8, 0, 6, 0, 0, 4},
+                    { 1, 3, 0, 0, 0, 0, 0, 8, 2},
+                    { 7, 0, 0, 2, 0, 3, 0, 0, 6},
+                    { 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                });
+
+            Solve_WithGivenPuzzle_SolvesPuzzle(puzzle);
+        }
+
+        private void Solve_WithGivenPuzzle_SolvesPuzzle(Puzzle puzzle)
+        {
             var solver = new PuzzleSolver(puzzle);
             var acts = new ActOnAllSegments(puzzle);
             var printPuzzle = new PrintPuzzle(puzzle);
