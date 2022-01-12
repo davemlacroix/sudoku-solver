94af2400641b5b0367f11f6f646d045d32f7e639
davem <davemlacroix@gmail.com>
2022-01-12 18:15:01 -0500

    add a .net core web api project

---
+++ SudokuSolver.Api/Controllers/SudokuPuzzleController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SudokuSolver.Api.Controllers
+{
+    [ApiController]
+    [Route("api/puzzle")]
+    public class SudokuPuzzleController : ControllerBase
+    {
+        
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult Get()
+        {
+            return Ok("Hello World");
+        }
+        
+    }
+}
