a0861034cff805280cf7e3d01d45b84b324878a9
davem <davemlacroix@gmail.com>
2022-03-31 19:35:11 -0400

    add logic for mapping class

--- SudokuSolver.Api/Services/SudokuPuzzleMapper.cs
+++ SudokuSolver.Api/Services/SudokuPuzzleMapper.cs
@@ -6,18 +6,46 @@ namespace SudokuSolver.Api.Services
 {
     public class SudokuPuzzleMapper
     {
-        public SudokuPuzzleMapper ()
+        public SudokuPuzzleMapper()
         {
         }
 
         public Puzzle ConvertApiModelToInternalModel (SudokuPuzzleModel apiModel)
         {
-            return new Puzzle();
+            int[,] puzzle = new int[9, 9];
+            
+            for(int i=0; i<9; i++)
+            {
+                for(int j=0; j<9; j++)
+                {
+                    puzzle[i,j] = apiModel.Puzzle[i][j].Value ?? 0;
+                }
+            }
+            
+            return new Puzzle(puzzle);
         }
 
         public SudokuPuzzleModel ConvertInteralModelToApiModel (Puzzle internalModel)
         {
-            return new SudokuPuzzleModel();
+            var puzzle = new SudokuPuzzleModel();
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    int? value = null;
+                    if (internalModel.GetCell(i, j).Value != 0 )
+                    {
+                        value = internalModel.GetCell(i, j).Value;
+                    }
+
+                    puzzle.Puzzle[i][j] = new SudokuCellModel()
+                    {
+                        Value = value
+                    };
+                }
+            }
+
+            return puzzle;
         }
 
     }
