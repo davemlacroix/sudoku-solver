df95db48ea1d477f76dd80449eb65e90b77f7227
davem <davemlacroix@gmail.com>
2022-03-31 20:48:08 -0400

    move puzzle retrieval to a gateway service

--- SudokuSolver.Api/Controllers/SudokuPuzzleController.cs
+++ SudokuSolver.Api/Controllers/SudokuPuzzleController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Primitives;
 using SudokuSolver.Api.Models;
+using SudokuSolver.Api.Services;
 
 namespace SudokuSolver.Api.Controllers
 {
@@ -30,117 +31,9 @@ namespace SudokuSolver.Api.Controllers
         [ProducesResponseType(StatusCodes.Status200OK)]
         public IActionResult Get()
         {
-            SudokuPuzzleModel puzzle = GetSudokuPuzzle();
+            SudokuPuzzleModel puzzle = new SudokuPuzzleGateway().GetPuzzle(0);
             return Ok(puzzle);
         }
 
-        private SudokuPuzzleModel GetSudokuPuzzle()
-        {
-            return new SudokuPuzzleModel
-            {
-                Puzzle = new SudokuCellModel[][]
-                {
-                    new SudokuCellModel[9] {
-                        new SudokuCellModel { Value = null },
-                        new SudokuCellModel { Value = null },
-                        new SudokuCellModel { Value = 3 },
-                        new SudokuCellModel { Value = null },
-                        new SudokuCellModel { Value = 2 },
-                        new SudokuCellModel { Value = null },
-                        new SudokuCellModel { Value = 6 },
-                        new SudokuCellModel { Value = null },
-                        new SudokuCellModel { Value = null }
-                    },
-                    new SudokuCellModel[9] {
-                        new SudokuCellModel { Value = 9 },
-                        new SudokuCellModel { Value = null },
-                        new SudokuCellModel { Value = null },
-                        new SudokuCellModel { Value = 3 },
-                        new SudokuCellModel { Value = null },
-                        new SudokuCellModel { Value = 5 },
-                        new SudokuCellModel { Value = null },
-                        new SudokuCellModel { Value = null },
-                        new SudokuCellModel { Value = 1 }
-                    },
-                    new SudokuCellModel[9] {
-                        new SudokuCellModel { Value = null },
-                        new SudokuCellModel { Value = null },
-                        new SudokuCellModel { Value = 1 },
-                        new SudokuCellModel { Value = 8 },
-                        new SudokuCellModel { Value = null },
-                        new SudokuCellModel { Value = 6 },
-                        new SudokuCellModel { Value = 4 },
-                        new SudokuCellModel { Value = null },
-                        new SudokuCellModel { Value = null }
-                    },
-                    new SudokuCellModel[9] {
-                        new SudokuCellModel { Value = null },
-                        new SudokuCellModel { Value = null },
-                        new SudokuCellModel { Value = 8 },
-                        new SudokuCellModel { Value = 1 },
-                        new SudokuCellModel { Value = null },
-                        new SudokuCellModel { Value = 2 },
-                        new SudokuCellModel { Value = 9 },
-                        new SudokuCellModel { Value = null },
-                        new SudokuCellModel { Value = null }
-                    },
-                    new SudokuCellModel[9] {
-                        new SudokuCellModel { Value = 7 },
-                        new SudokuCellModel { Value = null },
-                        new SudokuCellModel { Value = null },
-                        new SudokuCellModel { Value = null },
-                        new SudokuCellModel { Value = null },
-                        new SudokuCellModel { Value = null },
-                        new SudokuCellModel { Value = null },
-                        new SudokuCellModel { Value = null },
-                        new SudokuCellModel { Value = 8 }
-                    },
-                    new SudokuCellModel[9] {
-                        new SudokuCellModel { Value = null },
-                        new SudokuCellModel { Value = null },
-                        new SudokuCellModel { Value = 6 },
-                        new SudokuCellModel { Value = 7 },
-                        new SudokuCellModel { Value = null },
-                        new SudokuCellModel { Value = 8 },
-                        new SudokuCellModel { Value = 2 },
-                        new SudokuCellModel { Value = null },
-                        new SudokuCellModel { Value = null }
-                    },
-                    new SudokuCellModel[9] {
-                        new SudokuCellModel { Value = null },
-                        new SudokuCellModel { Value = null },
-                        new SudokuCellModel { Value = 2 },
-                        new SudokuCellModel { Value = 6 },
-                        new SudokuCellModel { Value = null },
-                        new SudokuCellModel { Value = 9 },
-                        new SudokuCellModel { Value = 5 },
-                        new SudokuCellModel { Value = null },
-                        new SudokuCellModel { Value = null }
-                    },
-                    new SudokuCellModel[9] {
-                        new SudokuCellModel { Value = 8 },
-                        new SudokuCellModel { Value = null },
-                        new SudokuCellModel { Value = null },
-                        new SudokuCellModel { Value = 2 },
-                        new SudokuCellModel { Value = null },
-                        new SudokuCellModel { Value = 3 },
-                        new SudokuCellModel { Value = null },
-                        new SudokuCellModel { Value = null },
-                        new SudokuCellModel { Value = 9 }
-                    },
-                    new SudokuCellModel[9] {
-                        new SudokuCellModel { Value = null },
-                        new SudokuCellModel { Value = null },
-                        new SudokuCellModel { Value = 5 },
-                        new SudokuCellModel { Value = null },
-                        new SudokuCellModel { Value = 1 },
-                        new SudokuCellModel { Value = null },
-                        new SudokuCellModel { Value = 3 },
-                        new SudokuCellModel { Value = null },
-                        new SudokuCellModel { Value = null }
-                    }
-                }
-            };
-        }
     }
 }
---
+++ SudokuSolver.Api/Services/SudokuPuzzleGateway.cs
@@ -0,0 +1,352 @@
+﻿using SudokuSolver.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SudokuSolver.Api.Services
+{
+    public class SudokuPuzzleGateway
+    {
+        public SudokuPuzzleModel GetPuzzle(int id)
+        {
+            switch (id)
+            {
+                case 0:
+                    return GetSudokuPuzzle0();
+                case 1:
+                    return GetSudokuPuzzle1();
+                case 2:
+                    return GetSudokuPuzzle2();
+                default:
+                    return null;
+            }
+        }
+
+
+        private SudokuPuzzleModel GetSudokuPuzzle0()
+        {
+            return new SudokuPuzzleModel
+            {
+                Puzzle = new SudokuCellModel[][]
+                {
+                    new SudokuCellModel[9] {
+                        new SudokuCellModel { Value = null },
+                        new SudokuCellModel { Value = null },
+                        new SudokuCellModel { Value = 3 },
+                        new SudokuCellModel { Value = null },
+                        new SudokuCellModel { Value = 2 },
+                        new SudokuCellModel { Value = null },
+                        new SudokuCellModel { Value = 6 },
+                        new SudokuCellModel { Value = null },
+                        new SudokuCellModel { Value = null }
+                    },
+                    new SudokuCellModel[9] {
+                        new SudokuCellModel { Value = 9 },
+                        new SudokuCellModel { Value = null },
+                        new SudokuCellModel { Value = null },
+                        new SudokuCellModel { Value = 3 },
+                        new SudokuCellModel { Value = null },
+                        new SudokuCellModel { Value = 5 },
+                        new SudokuCellModel { Value = null },
+                        new SudokuCellModel { Value = null },
+                        new SudokuCellModel { Value = 1 }
+                    },
+                    new SudokuCellModel[9] {
+                        new SudokuCellModel { Value = null },
+                        new SudokuCellModel { Value = null },
+                        new SudokuCellModel { Value = 1 },
+                        new SudokuCellModel { Value = 8 },
+                        new SudokuCellModel { Value = null },
+                        new SudokuCellModel { Value = 6 },
+                        new SudokuCellModel { Value = 4 },
+                        new SudokuCellModel { Value = null },
+                        new SudokuCellModel { Value = null }
+                    },
+                    new SudokuCellModel[9] {
+                        new SudokuCellModel { Value = null },
+                        new SudokuCellModel { Value = null },
+                        new SudokuCellModel { Value = 8 },
+                        new SudokuCellModel { Value = 1 },
+                        new SudokuCellModel { Value = null },
+                        new SudokuCellModel { Value = 2 },
+                        new SudokuCellModel { Value = 9 },
+                        new SudokuCellModel { Value = null },
+                        new SudokuCellModel { Value = null }
+                    },
+                    new SudokuCellModel[9] {
+                        new SudokuCellModel { Value = 7 },
+                        new SudokuCellModel { Value = null },
+                        new SudokuCellModel { Value = null },
+                        new SudokuCellModel { Value = null },
+                        new SudokuCellModel { Value = null },
+                        new SudokuCellModel { Value = null },
+                        new SudokuCellModel { Value = null },
+                        new SudokuCellModel { Value = null },
+                        new SudokuCellModel { Value = 8 }
+                    },
+                    new SudokuCellModel[9] {
+                        new SudokuCellModel { Value = null },
+                        new SudokuCellModel { Value = null },
+                        new SudokuCellModel { Value = 6 },
+                        new SudokuCellModel { Value = 7 },
+                        new SudokuCellModel { Value = null },
+                        new SudokuCellModel { Value = 8 },
+                        new SudokuCellModel { Value = 2 },
+                        new SudokuCellModel { Value = null },
+                        new SudokuCellModel { Value = null }
+                    },
+                    new SudokuCellModel[9] {
+                        new SudokuCellModel { Value = null },
+                        new SudokuCellModel { Value = null },
+                        new SudokuCellModel { Value = 2 },
+                        new SudokuCellModel { Value = 6 },
+                        new SudokuCellModel { Value = null },
+                        new SudokuCellModel { Value = 9 },
+                        new SudokuCellModel { Value = 5 },
+                        new SudokuCellModel { Value = null },
+                        new SudokuCellModel { Value = null }
+                    },
+                    new SudokuCellModel[9] {
+                        new SudokuCellModel { Value = 8 },
+                        new SudokuCellModel { Value = null },
+                        new SudokuCellModel { Value = null },
+                        new SudokuCellModel { Value = 2 },
+                        new SudokuCellModel { Value = null },
+                        new SudokuCellModel { Value = 3 },
+                        new SudokuCellModel { Value = null },
+                        new SudokuCellModel { Value = null },
+                        new SudokuCellModel { Value = 9 }
+                    },
+                    new SudokuCellModel[9] {
+                        new SudokuCellModel { Value = null },
+                        new SudokuCellModel { Value = null },
+                        new SudokuCellModel { Value = 5 },
+                        new SudokuCellModel { Value = null },
+                        new SudokuCellModel { Value = 1 },
+                        new SudokuCellModel { Value = null },
+                        new SudokuCellModel { Value = 3 },
+                        new SudokuCellModel { Value = null },
+                        new SudokuCellModel { Value = null }
+                    }
+                }
+            };
+        }
+        private SudokuPuzzleModel GetSudokuPuzzle1()
+        {
+            return new SudokuPuzzleModel
+            {
+                Puzzle = new SudokuCellModel[][]
+                {
+                        new SudokuCellModel[9] {
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = 3 },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = 2 },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = 6 },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = null }
+                        },
+                        new SudokuCellModel[9] {
+                            new SudokuCellModel { Value = 9 },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = 3 },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = 5 },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = 1 }
+                        },
+                        new SudokuCellModel[9] {
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = 1 },
+                            new SudokuCellModel { Value = 8 },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = 6 },
+                            new SudokuCellModel { Value = 4 },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = null }
+                        },
+                        new SudokuCellModel[9] {
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = 8 },
+                            new SudokuCellModel { Value = 1 },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = 2 },
+                            new SudokuCellModel { Value = 9 },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = null }
+                        },
+                        new SudokuCellModel[9] {
+                            new SudokuCellModel { Value = 7 },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = 8 }
+                        },
+                        new SudokuCellModel[9] {
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = 6 },
+                            new SudokuCellModel { Value = 7 },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = 8 },
+                            new SudokuCellModel { Value = 2 },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = null }
+                        },
+                        new SudokuCellModel[9] {
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = 2 },
+                            new SudokuCellModel { Value = 6 },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = 9 },
+                            new SudokuCellModel { Value = 5 },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = null }
+                        },
+                        new SudokuCellModel[9] {
+                            new SudokuCellModel { Value = 8 },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = 2 },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = 3 },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = 9 }
+                        },
+                        new SudokuCellModel[9] {
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = 5 },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = 1 },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = 3 },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = null }
+                        }
+                }
+            };
+        }
+        private SudokuPuzzleModel GetSudokuPuzzle2()
+        {
+            return new SudokuPuzzleModel
+            {
+                Puzzle = new SudokuCellModel[][]
+                {
+                        new SudokuCellModel[9] {
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = 3 },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = 2 },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = 6 },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = null }
+                        },
+                        new SudokuCellModel[9] {
+                            new SudokuCellModel { Value = 9 },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = 3 },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = 5 },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = 1 }
+                        },
+                        new SudokuCellModel[9] {
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = 1 },
+                            new SudokuCellModel { Value = 8 },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = 6 },
+                            new SudokuCellModel { Value = 4 },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = null }
+                        },
+                        new SudokuCellModel[9] {
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = 8 },
+                            new SudokuCellModel { Value = 1 },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = 2 },
+                            new SudokuCellModel { Value = 9 },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = null }
+                        },
+                        new SudokuCellModel[9] {
+                            new SudokuCellModel { Value = 7 },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = 8 }
+                        },
+                        new SudokuCellModel[9] {
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = 6 },
+                            new SudokuCellModel { Value = 7 },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = 8 },
+                            new SudokuCellModel { Value = 2 },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = null }
+                        },
+                        new SudokuCellModel[9] {
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = 2 },
+                            new SudokuCellModel { Value = 6 },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = 9 },
+                            new SudokuCellModel { Value = 5 },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = null }
+                        },
+                        new SudokuCellModel[9] {
+                            new SudokuCellModel { Value = 8 },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = 2 },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = 3 },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = 9 }
+                        },
+                        new SudokuCellModel[9] {
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = 5 },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = 1 },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = 3 },
+                            new SudokuCellModel { Value = null },
+                            new SudokuCellModel { Value = null }
+                        }
+                }
+            };
+        }
+    }
+}
